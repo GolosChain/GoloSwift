abe00a0ae2a60e9a83dcbf79ac336b63fb457b69
SerhiiM <msm72@mail.ru>
2018-06-20 11:37:29 +0300

    Add String extension: transliterate Cyrillic -> Latin.

--- GoloSwift/Extensions/String+Extensions.swift
+++ GoloSwift/Extensions/String+Extensions.swift
@@ -36,4 +36,29 @@ extension String {
     public var base58EncodedString: String {
         return [Byte](utf8).base58EncodedString
     }
+    
+    
+    /// Cyrillic -> Latin
+    func transliterationInLatin() -> String {
+        var newString: String = ""
+        var latinChar: String
+        
+        for char in self {
+            latinChar = transliterate(char: "\(char)")
+            newString.append(latinChar)
+        }
+        
+        return String(format: "ru-%@", newString)
+    }
+    
+    func transliterate(char: String) -> String {
+        let cyrillicChars   =   ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
+        
+        // https://en.wikipedia.org/wiki/Romanization_of_Russian, Passport (2013), ICAO
+        let latinChars      =   ["a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "'", "e", "iu", "ia"]
+        
+        let convertDict     =   NSDictionary.init(objects: latinChars, forKeys: cyrillicChars as [NSCopying])
+        
+        return  convertDict.value(forKey: char.lowercased()) as! String
+    }
 }
