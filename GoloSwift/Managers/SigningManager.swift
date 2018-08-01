99dd50d14e245f645472fa22a2b90156d3e830be
SerhiiM <msm72@mail.ru>
2018-08-01 10:39:29 +0300

    Add SigningManager.

---
+++ GoloSwift/Managers/SigningManager.swift
@@ -0,0 +1,64 @@
+//
+//  SigningManager.swift
+//  GoloSwift
+//
+//  Created by msm72 on 01.08.2018.
+//  Copyright © 2018 golos. All rights reserved.
+//
+
+import secp256k1
+import Foundation
+import CryptoSwift
+
+public class Signing {
+    /**
+     ECC signing serialized buffer of transaction.
+     
+     - Parameter method: The name of used API method with needed parameters.
+     - Returns: Return transaction signature.
+     
+     */
+    public static func signingECC(messageSHA256: [Byte]) -> String {
+        if let privateKeyString = KeychainManager.loadPrivateKey(forUserName: self.userName) {
+            let privateKeyData: [Byte] =  GSBase58().base58Decode(data: privateKeyString)
+            
+            Logger.log(message: "\nsigningECC - privateKey:\n\t\(privateKeyString)\n", event: .debug)
+            Logger.log(message: "\nsigningECC - privateKeyData:\n\t\(privateKeyData.toHexString())\n", event: .debug)
+            
+            var index: Int = 0
+            var extra: [Byte]?
+            var loopCounter: Byte = 0
+            var recoveryID: Int32 = 0
+            var isRecoverable: Bool = false
+            var signature = secp256k1_ecdsa_recoverable_signature()             // sig
+            let ctx: OpaquePointer = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))
+            
+            while (!(isRecoverable && isCanonical(signature: signature))) {
+                if (loopCounter == 0xff) {
+                    index += 1
+                    loopCounter = 0
+                }
+                
+                if (loopCounter > 0) {
+                    extra = [Byte].add(randomElementsCount: 32)             // new bytes[32]
+                }
+                
+                loopCounter += 1
+                isRecoverable = (secp256k1_ecdsa_sign_recoverable(ctx, &signature, messageSHA256, privateKeyData, nil, extra) as NSNumber).boolValue
+                Logger.log(message: "\nsigningECC - extra:\n\t\(String(describing: extra?.toHexString()))\n", event: .debug)
+                Logger.log(message: "\nsigningECC - sig.data:\n\t\(signature.data))\n", event: .debug)
+            }
+            
+            var output65: [Byte] = [Byte].init(repeating: 0, count: 65)     // add(randomElementsCount: 65)         // new bytes[65]
+            secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, &output65[1], &recoveryID, &signature)
+            
+            // 4 - compressed | 27 - compact
+            Logger.log(message: "\nsigningECC - output65-1:\n\t\(output65.toHexString())\n", event: .debug)
+            output65[0] = Byte(recoveryID + 4 + 27)                             // (byte)(recoveryId + 4 + 27)
+            Logger.log(message: "\nsigningECC - output65-2:\n\t\(output65.toHexString())\n", event: .debug)            
+            Logger.log(message: "\ntx - ready:\n\t\(self)\n", event: .debug)
+        }
+        
+        return output65.toHexString()
+    }
+}
--- GoloSwift/Models/Attachment.swift
+++ GoloSwift/Models/Attachment.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import secp256k1
+import CryptoSwift
 
 public enum AttachmentType {
     case link
@@ -27,5 +29,28 @@ public struct Attachment {
         self.range      =   range
         self.value      =   value
         self.type       =   type
+    }
+    
+    
+    // MARK: - Custom Functions
+    public func createURL(forImage: UIImage) -> String {
+        // Create prefix
+        let imagePrefix: [Byte]     =   "ImageSigningChallenge".hexBytes
+        Logger.log(message: "\nimagePrefix:\n\t\(imagePrefix.toHexString())\n", event: .debug)
+
+        // Concatenation bytes
+        let imageData: [Byte]       =   Data(base64Encoded: UIImagePNGRepresentation(image))
+        let concatenation: [Byte]   =   imagePrefix.append(contentsOf: imageData)
+        Logger.log(message: "\nconcatenation:\n\t\(concatenation.toHexString())\n", event: .debug)
+
+        // SHA256
+        let concatenationSHA256: [Byte]     =   concatenation.sha256()
+        Logger.log(message: "\nconcatenationSHA256:\n\t\(concatenationSHA256.toHexString())\n", event: .debug)
+        
+        // ECC signing
+        let signature   =   SigningManager.signingECC(messageSHA256: concatenationSHA256)
+        Logger.log(message: "\nsignature:\n\t\(signature)\n", event: .debug)
+
+        return signature
     }
 }
